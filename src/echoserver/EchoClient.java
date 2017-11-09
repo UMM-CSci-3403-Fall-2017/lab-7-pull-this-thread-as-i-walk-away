a4bb8a2ef98b58b0999188c66da71a9b0fb23b60
casey369 <casey369@morris.umn.edu>
2017-11-09 11:30:34 -0600

    Modify EchoClient.java to use threads

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,12 +1,67 @@
 package echoserver;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class EchoClient {
 	public static final int PORT_NUMBER = 6013;
+	
+	public static class ServerWriter extends Thread{
+		OutputStream out;
+		Socket socket;
+		
+		public ServerWriter(Socket socket) throws IOException{
+			this.socket = socket;
+			out = socket.getOutputStream();
+		}
+		
+		public void run(){
+			InputStream stdIn = new DataInputStream(System.in);
+			byte[] input = new byte[1];
+			try {
+				while(stdIn.read(input) != -1){
+					out.write(input);
+				}
+				this.socket.shutdownOutput();
+				out.close();
+				stdIn.close();
+			} catch (IOException e) {
+				System.out.println("Ayy lmao how did I end up here. :(");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static class ServerReader extends Thread{
+		InputStream in;
+		Socket socket;
+		
+		public ServerReader(Socket socket) throws IOException{
+			this.socket = socket;
+			in = socket.getInputStream();
+		}
+		
+		public void run(){
+			byte[] output = new byte[1];
+			try {
+				while(in.read(output) != -1){
+					System.out.write(output, 0, 1);
+				}
+				System.out.flush();
+				socket.close();
+				in.close();
+			} catch (IOException e) {
+				System.out.println("Ayy lmao how did I end up here. :(");
+				e.printStackTrace();
+			}
+			
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		EchoClient client = new EchoClient();
@@ -15,14 +70,11 @@ public class EchoClient {
 
 	private void start() throws IOException {
 		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
-		int readByte;
-		while ((readByte = System.in.read()) != -1) {
-			socketOutputStream.write(readByte);
-			int socketByte = socketInputStream.read();
-			System.out.write(socketByte);
-		}
+		ServerReader reader = new ServerReader(socket);
+		ServerWriter writer = new ServerWriter(socket);
+		reader.start();
+		writer.start();
 		System.out.flush();
 	}
+	
 }
