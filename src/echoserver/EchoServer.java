e9e196a32e12a5ca7c0ef54484bf29ffe6065ada
Philip Blaskowski <blask017@morris.umn.edu>
2017-11-10 19:03:28 -0600

    Modify EchoServer.java to hopefully handle Threads

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -5,9 +5,33 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class EchoServer {
 	public static final int PORT_NUMBER = 6013;
+	
+	private static class ClientWriter extends Thread{
+		private InputStream in;
+		private OutputStream out;
+		
+		public ClientWriter(Socket socket) throws IOException{
+			this.in = socket.getInputStream();
+			this.out = socket.getOutputStream();
+		}
+		
+		public void run(){
+			byte[] output = new byte[1];
+			try {
+				while(in.read(output) != -1){
+					out.write(output);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
@@ -16,14 +40,11 @@ public class EchoServer {
 
 	private void start() throws IOException, InterruptedException {
 		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+		ExecutorService executor = Executors.newCachedThreadPool();
 		while (true) {
-			Socket socket = serverSocket.accept();
-			InputStream inputStream = socket.getInputStream();
-			OutputStream outputStream = socket.getOutputStream();
-			int b;
-			while ((b = inputStream.read()) != -1) {
-				outputStream.write(b);
-			}
+			ClientWriter writer = new ClientWriter(serverSocket.accept());
+			executor.execute(writer);
 		}
 	}
+	
 }
